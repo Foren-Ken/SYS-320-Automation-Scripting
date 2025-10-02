f8e165ee30bdc9962e95a77e6c3d5d396e7a3508
Kenneth <143446633+Foren-Ken@users.noreply.github.com>
2025-10-01 20:43:32 -0400

    Week4-3 Update

---
+++ week4-Parsing Apache Logs/ApacheSkibidiLogs.ps1
@@ -0,0 +1,23 @@
+﻿function ApacheLogs1() {
+$unformattedLogs = Get-Content C:\xampp\apache\logs\access.log
+$deliverable = @()
+
+for($x=0; $x -lt 1<#$unformattedLogs.Length#>; $x++){
+$x
+$chopper = $unformattedLogs[$x].Split(" ")
+# $chopper
+
+$deliverable += [pscustomobject]@{"IP" = $chopper[0];
+                                  "Time"= $chopper[3].Trim("[");
+                                  "Method" = $chopper[5].Trim('"');
+                                  "Page" = $chopper[6];
+                                  "Protocol" = $chopper[7].Trim('"');
+                                  "Response" = $chopper[8];
+                                  "Referrer" = $chopper[10];
+                                  "Client" =  $chopper[11..($chopper.Count)];}
+
+}
+return $deliverable | Where-Object {$_.IP -ilike "10.*"}
+}
+$deliverable = ApacheLogs1
+$deliverable | Format-Table -Autosize -Wrap
