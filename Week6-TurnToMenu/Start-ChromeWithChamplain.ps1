17a8ad4e8e3a231af38337921d4d7dad60626e53
Kenneth <143446633+Foren-Ken@users.noreply.github.com>
2025-10-09 00:13:57 -0400

    Week6 Part 2

---
+++ Week6-TurnToMenu/Start-ChromeWithChamplain.ps1
@@ -0,0 +1,11 @@
+﻿function startChamplainChrome () {
+$processList = Get-Process | Where-Object {$_.ProcessName -ilike "chrome"}
+if ($processList.Count -eq 0){
+    Start-Process "https://Champlain.edu"
+}
+ else {
+    Stop-Process $processList
+}
+# Use the following to check if its working. 
+# Write-Host $processList
+}
---
+++ Week6-TurnToMenu/main.ps1
@@ -0,0 +1,66 @@
+﻿. (Join-Path $PSScriptRoot ApacheSkibidiLogs.ps1)
+. (Join-Path $PSScriptRoot Start-ChromeWithChamplain.ps1)
+. (Join-Path $PSScriptRoot Users.ps1)
+. (Join-Path $PSScriptRoot Event-Logs.ps1)
+
+clear
+
+$menu = "`n"
+$menu += "1 - Display last 10 Apache Logs`n"
+$menu += "2 - Display last 10 failed Logins for all Users`n"
+$menu += "3 - Display at risk users`n"
+$menu += "4 - Start Chrome web browser and naviagte to Champlain.edu`n"
+$menu += "5 - Exit program`n"
+
+While ($true) {
+    Write-Host $menu | Out-String
+    $choice = Read-Host
+
+    if ($choice -eq 1){
+    ApacheLogs1 | Select-Object -last 10 | Format-Table | Out-String
+    
+    }
+    elseif ($choice -eq 2) {
+    Write-Host "How many days ago for these logs?"
+    $days = Read-Host
+
+        if ($days -match "^\d+$"){
+    Write-Host "Proper input detected. Processing..."
+    }
+        else {
+    Write-Host "Thats no valid integer :("
+    continue
+    }
+
+    getFailedLogins($days) | Select-Object -first 10 | Format-Table | Out-String
+    
+    }
+
+    elseif ($choice -eq 3){
+    Write-Host "How many days ago for these logs?"
+    $days = Read-Host
+
+        if ($days -match "^\d+$"){
+    Write-Host "Proper input detected. Processing..."
+    }
+        else {
+    Write-Host "Thats no valid integer :("
+    continue
+    }
+
+    $results = atRiskUsers($days)
+    Write-Host ($results | Format-Table | Out-string)
+    }
+
+    elseif ($choice -eq 4){
+    startChamplainChrome
+    }
+
+    elseif ($choice -eq 5){
+    exit
+    }
+
+    else {
+    Write-Host "$choice is not a valid option, choose an integer between 1-5"
+    }
+}
